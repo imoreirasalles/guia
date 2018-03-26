486b16bf5dabba3ce19ad87f80483692337f14c7
Photonauta 35 <felipehistory@gmail.com>
2018-03-26 14:17:04 -0300

    rebuilding the whole project

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', views.ColecaoListView.as_view(), name='colecoes'),
-	path('colecao/<str:pk>', views.ColecaoDetailView.as_view(), name='colecao-detalhe'),
-	path('exhibitions/', views.ColecaoListView.as_view(), name='exhibitions'),
-]
