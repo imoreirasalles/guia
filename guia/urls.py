9d8db09ec4fcb5613e24908cabd564df23fade85
Photonauta 35 <felipehistory@gmail.com>
2018-06-07 11:33:03 -0300

    adding new base and new home django template

--- guia/urls.py
+++ guia/urls.py
@@ -7,5 +7,6 @@ from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/', base),
     path('', home),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
