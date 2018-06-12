40f88899ff3fe3f42925a58aa73df5ec5fcf9024
Photonauta 35 <felipehistory@gmail.com>
2018-06-12 18:19:42 -0300

    update internal home templates

--- guia/views.py
+++ guia/views.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.utils import timezone
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import login_required
-
+from core.models import *
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -16,6 +19,26 @@ def home(request):
 
 def collection(request):
     return render(request, 'collection.html')
+
+
+class CollectionList(ListView):
+    model = Collection
+    paginate_by = 30
+    template_name = "collection.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+class CollectionDetail(DetailView):
+    model = Collection
+    template_name = "collection_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
 
 
 def exhibition(request):
