e4aa26eb173994753ed5a68407d372b4ce4ef650
Laury Bueno <laury@hacklab.com.br>
2018-06-18 16:06:40 -0300

    [Dev] Temporary change on folder configuration for local static files

--- guia/settings.py
+++ guia/settings.py
@@ -132,7 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
 
 MEDIA_URL   = '/media/'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
