be98d1b231ea8c5c624a5633e3a3ce4676c844bd
Laury Bueno <laury@hacklab.com.br>
2018-06-18 13:18:09 -0300

    Media root path should be determined using de BASE_DIR method

--- guia/settings.py
+++ guia/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL   = '/media/'
-MEDIA_ROOT  = '/app/media/'
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 
 # Sentry Configuration
