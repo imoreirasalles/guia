7976e5ce4ee94a8243ea510806fd27c2f03955d1
Photonauta 35 <felipehistory@gmail.com>
2018-06-22 17:29:56 -0300

    adding new extesion into docker instructions

--- compose/local/postgres/init.sh
+++ compose/local/postgres/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+apt update
+apt install postgresql postgresql-contrib postgis postgresql-9.6-postgis-2.3 postgresql-9.6-postgis-2.3-scripts
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
-    CREATE EXTENSION IF NOT EXISTS hstore;
+    CREATE EXTENSION IF NOT EXISTS postgis;
 EOSQL
