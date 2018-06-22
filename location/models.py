095286e8b5f016d0d5fc012cfc6902cfaf6def25
Photonauta 35 <felipehistory@gmail.com>
2018-06-22 17:29:20 -0300

    updating location model

--- location/models.py
+++ location/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Location(models.Model):
+    """store places addresses, and link places with art works, sets, collection, persons, etc"""
+    title = models.CharField(max_length=128, null=False, blank=True)
+    street = models.CharField(max_length=128, null=False, blank=True)
+    number = models.CharField(max_length=32, null=False, blank=True)
+    complement = models.CharField(max_length=128, null=False, blank=True)
+    neighborhood = models.CharField(max_length=64, null=False, blank=True)
+    state = models.CharField(max_length=64, null=False, blank=True)
+    city = models.CharField(max_length=64, null=False, blank=True)
+    country = models.CharField(max_length=64, null=False, blank=True)
+    postal_code = models.CharField(max_length=32, null=False, blank=True)
+    lat_and_long = models.PointField(null=False, blank=True)
+
+    def __str__(self):
+        return self.title
