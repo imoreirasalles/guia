e8428ad2f2f5f7816c7c58c27002809157a1e7ab
Photonauta 35 <felipehistory@gmail.com>
2018-06-25 18:21:13 -0300

    adding gdal packages into docker script

--- compose/local/django/runserver.sh
+++ compose/local/django/runserver.sh
@@ -5,6 +5,8 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
+apt update
+apt-get install gdal-bin libgdal-dev libgdal-doc libgdal-grass python3-gdal
 
 python manage.py migrate
 python manage.py runserver_plus 0.0.0.0:8000
