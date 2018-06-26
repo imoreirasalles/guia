dcc2dde61535d7258d01e00353455a5404ba7c38
vitor-kato <vitor.s.kato@gmail.com>
2018-06-26 17:25:56 -0300

    Fixes Docker for postgres image with postgis

--- compose/local/postgres/init.sh
+++ compose/local/postgres/init.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-apt update
-apt install postgresql postgresql-contrib postgis postgresql-9.6-postgis-2.3 postgresql-9.6-postgis-2.3-scripts
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS postgis;
 EOSQL
