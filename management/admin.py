fcfe40e3a7030f65edfd7f16c065e13844931abb
Photonauta 35 <felipehistory@gmail.com>
2018-07-03 18:49:38 -0300

    adding Acquisition and AcquisitionMethod models

--- management/admin.py
+++ management/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(ManagementUnit)
+@admin.register(ManagementUnit)
+class ManagementUnitAdmin(admin.ModelAdmin):
+    readonly_fields = ['created']
+    list_display = ('id', 'title', 'description')
+
+
+@admin.register(AcquisitionMethod)
+class AcquisitionMethodAdmin(admin.ModelAdmin):
+    readonly_fields = ['created']
+    list_display = ('id', 'title', 'description')
+
+
+@admin.register(Acquisition)
+class AcquisitionAdmin(admin.ModelAdmin):
+    readonly_fields = ['created']
+    list_display = ('uuid', 'title', 'method', 'date_start', 'date_end')
