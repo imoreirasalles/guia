73a9d349f3141eac2d13c5ec539a89da7082ad06
Photonauta 35 <felipehistory@gmail.com>
2018-07-25 17:42:34 -0300

    [admin] reorder fields in Collection form using fieldset #53

--- collection/admin.py
+++ collection/admin.py
@@ -37,7 +37,7 @@ class CollectionResource(resources.ModelResource):
 class CollectionAdminForm(forms.ModelForm):
     id_old = forms.CharField(
         required=False,
-        widget=JSONEditorWidget(settings.DATA_SCHEMA, collapsed=False),
+        widget=JSONEditorWidget(settings.DATA_SCHEMA, collapsed=True),
         label=_('Old IDs'))
     full_text = forms.CharField(
         required=False,
@@ -45,15 +45,15 @@ class CollectionAdminForm(forms.ModelForm):
         label=_('Full Text'))
     dimensions = forms.CharField(
         required=False,
-        widget=JSONEditorWidget(settings.DATA_SCHEMA, collapsed=False),
+        widget=JSONEditorWidget(settings.DATA_SCHEMA, collapsed=True),
         label=_('Dimensions'))
     inventary_data = forms.CharField(
         required=False,
-        widget=JSONEditorWidget(settings.DATA_SCHEMA, collapsed=False),
+        widget=JSONEditorWidget(settings.DATA_SCHEMA, collapsed=True),
         label=_('Inventary Data'))
     other_data = forms.CharField(
         required=False,
-        widget=JSONEditorWidget(settings.DATA_SCHEMA, collapsed=False),
+        widget=JSONEditorWidget(settings.DATA_SCHEMA, collapsed=True),
         label=_('Other Unstructured Data'))
 
     def get_queryset(self):
@@ -72,4 +72,53 @@ class CollectionAdmin(CompareVersionAdmin, ImportExportModelAdmin, admin.ModelAd
     list_display = ('id_human', 'management_unit', 'aggregation_type', 'title', 'description_level', 'access_condition', 'inventary_status', 'items_total')
     search_fields = ['id_human', 'id_old', 'abstract', 'full_text', 'description_level__title', 'aggregation_type__title', 'genre_tags__title', 'dimensions', 'date_start', 'date_start_caption', 'date_end', 'date_end_caption', 'capture__title', 'author__title', 'container__title', 'access_condition__title', 'location_generic__title', 'management_unit__title', 'other_data', ]
     filter_horizontal = ('capture', 'author', 'container')
+    fieldsets = (
+        (_('Indentification'), {
+        'classes': ('wide','id_old_fix'),
+        'fields': ( ('uuid', 'created'),
+                    ('id_human', 'id_old'),
+                    ('management_unit', 'aggregation_type'),
+                    ('title', 'slug'),
+                    ('capture'),
+        ),}
+        ),
+        (_('Basic Infos'), {
+        'fields': ( ('date_start', 'date_start_caption'),
+                    ('date_end', 'date_end_caption'),
+                    ('access_condition', 'description_level'),
+                    ('genre_tags'),
+                    ('abstract'),
+        ),}
+        ),
+        (_('Location and Dimensions'), {
+        'fields': ( ('location_generic', 'location_specific'),
+                    ('dimensions'),
+        ),}
+        ),
+        (_('Inventory and Patrimony'), {
+        'fields': ( ('inventary_status', 'inventary_last_date'),
+                    ('inventary_data'),
+        ),}
+        ),
+        (_('Access'), {
+        'fields': ( ('access_local_status', ),
+                    ('access_online_status', 'access_link'),
+        ),}
+        ),
+        (_('Processed Status'), {
+        'fields': ( ('items_total', 'items_processed', 'items_online'),
+        ),}
+        ),
+
+        (_('Description'), {
+        'fields': ('full_text', 'container'),}
+        ),
+        (_('Other Infos'), {
+        'fields': ('other_data',),
+        }),
+    )
     form = CollectionAdminForm
+    class Media:
+        css = {
+             'all': ('css/guia.css',)
+        }
