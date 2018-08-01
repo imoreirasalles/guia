7109dbff3d1885e3f75329f79614570796359ebb
Photonauta 35 <felipehistory@gmail.com>
2018-08-01 18:02:40 -0300

    adding fixtures into docker-compose local script - #94

--- compose/local/django/runserver.sh
+++ compose/local/django/runserver.sh
@@ -8,4 +8,10 @@ set -o xtrace
 python manage.py migrate
 # Creates an superuser with login=admin password=test
 python manage.py loaddata superuser
+python manage.py loaddata glossary_AccessCondition_initial_data.json
+python manage.py loaddata glossary_AggregationType_initial_data.json
+python manage.py loaddata glossary_DescriptionLevel_initial_data.json
+python manage.py loaddata glossary_GenreTag_initial_data.json
+python manage.py loaddata location_Location_initial_data.json
+python manage.py loaddata management_ManagementUnit_initial_data.json
 python manage.py runserver_plus 0.0.0.0:8000
