0ece45eac0952e2894e7bac6dab8b7ab552518e4
Photonauta 35 <felipehistory@gmail.com>
2018-08-25 12:53:47 -0300

    adding new style to sidebar

---
+++ static/js/guia.js
@@ -0,0 +1,6 @@
+function mouseoutDisplayShow(id) {
+  document.getElementById(id).style.display = "block";
+}
+function mouseoverDisplayNone(id) {
+  document.getElementById(id).style.display = "none";
+}
