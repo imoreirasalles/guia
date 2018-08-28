2bc527cb6c1937a03cffcdd6933225e07d5ddbf2
Photonauta 35 <felipehistory@gmail.com>
2018-08-28 17:26:51 -0300

    getting data from back to front - #82

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
+# core django imports
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
 
-# Create your views here.
+# Project guia imports
+from .models import *
+
+
+class PostList(ListView):
+    model = Post
+    context_object_name = "post_list"
+    template_name = "home.html"
