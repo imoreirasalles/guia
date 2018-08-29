80aa1fd5efdf23da45a14d5b9e4a82855dc2e56b
photonauta35 <felipehistory@gmail.com>
2018-08-29 16:15:49 -0300

    adding collection filter

--- collection/views.py
+++ collection/views.py
@@ -9,6 +9,10 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Project guia imports
 from .models import *
+from glossary.models import DescriptionLevel
+from glossary.models import AccessCondition
+from glossary.models import GenreTag
+from management.models import ManagementUnit
 
 
 
@@ -27,9 +31,38 @@ class CollectionList(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
-        context['current_order'] = self.get_ordering()
-        context['order'] = self.order
+        qr                       = Collection.objects.all()
+
+        descriptionlevel_filter = self.request.GET.get('descriptionlevel')
+        if descriptionlevel_filter :
+            if int(descriptionlevel_filter) > 0:
+                qr = qr.filter(description_level=int(descriptionlevel_filter))
+
+        accesscondition_filter = self.request.GET.get('accesscondition')
+        if accesscondition_filter :
+            if int(accesscondition_filter) > 0:
+                qr = qr.filter(access_condition=int(accesscondition_filter))
+
+        genretag_filter = self.request.GET.get('genretag')
+        if genretag_filter :
+            if int(genretag_filter) > 0:
+                qr = qr.filter(genre_tags=int(genretag_filter))
+
+        managementunit_filter = self.request.GET.get('managementunit')
+        if managementunit_filter :
+            if int(managementunit_filter) > 0:
+                qr = qr.filter(management_unit=int(managementunit_filter))
+
+        context['list']          = qr.order_by(self.get_ordering())
+        
+        context['now']           = timezone.now()
+        context['order']         = self.order
+
+        context['descriptionlevel_list'] = DescriptionLevel.objects.filter().order_by()
+        context['accesscondition_list']  = AccessCondition.objects.filter().order_by()
+        context['genretag_list']         = GenreTag.objects.filter().order_by()
+        context['managementunit_list']   = ManagementUnit.objects.filter().order_by()
+
         return context
 
 
