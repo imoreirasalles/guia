d0742eef718fd01c5b0c7b009f4d0fbfce4b13cc
Photonauta 35 <felipehistory@gmail.com>
2018-09-04 17:28:48 -0300

    fix carousel aspect ration and data source. add modal to login (first step)

--- home/views.py
+++ home/views.py
@@ -15,3 +15,15 @@ class PostList(ListView):
     model = Post
     context_object_name = "post_list"
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if (Post.objects.order_by('id_auto_series').exists()):
+            post = Post.objects.latest('capture')
+            context['first_image_id'] = post.capture.first().id_auto_series
+            image_list = [str(index) for index in range(post.capture.count())]
+            context['image_list'] = image_list
+        else:
+            context['first_active_image'] = ''
+
+        return context
