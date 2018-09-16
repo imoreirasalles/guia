fd8aaf35694022878ae751644d7da9f08067f18e
Felipe Cabral <felipehistory@gmail.com>
2018-09-16 17:11:45 -0300

    change style of home card data. No multicolors, just the first one

--- collection/views.py
+++ collection/views.py
@@ -85,7 +85,8 @@ class CollectionDetail(DetailView):
 class CollectionSearchList(CollectionList):
     """docstring for [object Object]."""
     model = Collection
-    template_name = "collection_detail.html"    
+    paginate_by = 10
+    template_name = "collection_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
