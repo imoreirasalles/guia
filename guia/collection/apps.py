5a2b134b17e7e7adb50960dda6b87bcbae39807c
buccalon <buccalon@users.noreply.github.com>
2019-12-11 11:11:42 -0300

    add verbose_name and translation

--- guia/collection/apps.py
+++ guia/collection/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CollectionConfig(AppConfig):
     name = 'collection'
+    verbose_name = _('Collection App')
