1a02ba3b52408df82ca5dca1d849e7cd745f9c15
buccalon <buccalon@users.noreply.github.com>
2019-12-23 00:17:47 -0300

    add management_unit and acquisition_type as ForeignKey

--- guia/collection/admin.py
+++ guia/collection/admin.py
@@ -8,8 +8,8 @@ from .models import Collection
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ("label", "summary", "inventory_status", "contract_status",
-                    "insurance_status", "dimention_items", "published",)
-    list_filter = ('aggregation_type',)
+                    "insurance_status", "items_total", 'linear_meters', "published",)
+    list_filter = ('aggregation_type', 'management_unit', 'acquisition_type',)
     readonly_fields = ["created_at", "updated_at", "uuid", "slug", ]
     filter_horizontal = ('aggregation_type',)
     fieldsets = (
@@ -20,19 +20,25 @@ class CollectionAdmin(admin.ModelAdmin):
                      }),
                 (_('Full description'),
                     {'fields': (
-                        ('id_human'),
-                        ('aggregation_type'),
                         ('start_date', 'end_date'),
-                        ('article'))
+                        ('article'),
+                        ('aggregation_type'))
+                     }),
+                (_('Dimentions'),
+                    {'fields': (
+                        ('items_total', 'linear_meters')),
                      }),
                 (_('Progress Bar'),
                     {'fields': (
-                        ('progressbar_work', 'progressbar_online')),
+                        ('progress_technical', 'progress_online')),
                      }),
                 (_('Management'),
                     {'fields': (
-                        ('inventory_status', 'inventory_start_date', 'inventory_end_date'),
+                        ('id_human', 'management_unit'),
+                        ('acquisition_type'),
                         ('contract_status', 'contract_start_date', 'contract_end_date'),
-                        ('insurance_status', 'insurance_start_date', 'insurance_end_date')),
+                        ('inventory_status', 'inventory_start_date', 'inventory_end_date'),
+                        ('insurance_status', 'insurance_start_date', 'insurance_end_date'),
+                        ('private_notes',)),
                      }),
     )
